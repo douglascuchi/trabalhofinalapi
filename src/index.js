49b5a346a084f1757e82a6dbd4a00532853373d3
Douglas Eduardo <douglaszer0@live.com>
2023-05-18 21:08:33 -0300

    implementando estrutura

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => { res.send('API'); });
+
+app.listen(3000, () => {
+    console.log(`Aplicação rodando na porta ${3000}`);
+});
